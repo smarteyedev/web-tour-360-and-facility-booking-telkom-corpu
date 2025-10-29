d7e2b2829bd43c1380867c39368d752d8a88b34c
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-10-29 13:44:45 +0700

    initial-project

---
+++ Assets/Scripts/Controller/TourManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tour360TelkomCorpu
+{
+    public class TourManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log($"");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
