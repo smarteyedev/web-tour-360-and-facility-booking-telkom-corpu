ff546fd8f27d945398b7b810ea23ed4d8810f62a
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-11-24 08:54:40 +0700

    fix: bug-spam-hotspot

--- Assets/Scripts/Tour Manager/Hotspot Handler/HotspotHandler.cs
+++ Assets/Scripts/Tour Manager/Hotspot Handler/HotspotHandler.cs
@@ -56,6 +56,8 @@ namespace Tour360TelkomCorpu.HotspotHandler
         {
             if (_rectTransform == null)
                 _rectTransform = GetComponent<RectTransform>();
+
+            isInteractable = false;
         }
 
         protected override void Start()
@@ -162,6 +164,7 @@ namespace Tour360TelkomCorpu.HotspotHandler
             if (angle > maxVisibleAngle) { _canvasGroupHotspot.alpha = 0f; return; }
 
             _canvasGroupHotspot.alpha = 1f;
+            isInteractable = true;
 
             if (canvasRect != null && uiCanvas != null)
             {
