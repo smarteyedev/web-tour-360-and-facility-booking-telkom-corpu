e488a3c52697b92e93d727d7d2414622a4f29bd8
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-11-28 15:52:35 +0700

    update: camera-controller

--- Assets/Scripts/Tour Manager/Camera/CameraController.cs
+++ Assets/Scripts/Tour Manager/Camera/CameraController.cs
@@ -6,9 +6,6 @@ namespace Tour360TelkomCorpu.TourManager
     using System;
     using DG.Tweening;
     using Tour360TelkomCorpu.CanvasManager;
-
-
-
     public class CameraController : MonoBehaviour
     {
         [Header("Rotation")]
@@ -31,7 +28,6 @@ namespace Tour360TelkomCorpu.TourManager
         private bool applyingInertia = false;
 
         private Vector3 lastMousePos;
-
         private float verticalAngle = 0f;
 
         [Header("Auto Rotate")]
@@ -42,27 +38,27 @@ namespace Tour360TelkomCorpu.TourManager
         [SerializeField] private float autoRotateSpeed = 5f;
 
         [Header("Zoom")]
-        [SerializeField] private float minFOV;
-        [SerializeField] private float maxFOV;
+        [SerializeField] private float minFOV = 20f;
+        [SerializeField] private float maxFOV = 60f;
         [SerializeField] private float zoomSmooth = 6f;
-        [SerializeField] private float targetFOV;
+        private float targetFOV = 60f;
 
         [Header("Component References")]
         public Camera cam;
-
+        [SerializeField] private Slider zoomSlider;
         [SerializeField] private CanvasManager _canvasManager;
 
         void Start()
         {
             if (cam == null) cam = Camera.main;
-            
-            //if (zoomSlider != null)
-            //{
-            //    zoomSlider.minValue = 0;
-            //    zoomSlider.maxValue = 1;
-            //    targetFOV = Mathf.Lerp(minFOV, maxFOV, zoomSlider.value);
-            //    zoomSlider.onValueChanged.AddListener(OnZoomSliderChanged);
-            //}
+
+            if (zoomSlider != null)
+            {
+                zoomSlider.minValue = 0;
+                zoomSlider.maxValue = 1;
+                targetFOV = Mathf.Lerp(minFOV, maxFOV, zoomSlider.value);
+                zoomSlider.onValueChanged.AddListener(OnZoomSliderChanged);
+            }
 
             _canvasManager.SetupButtonAutoRotation(AutoRotationToggle);
         }
@@ -170,8 +166,8 @@ namespace Tour360TelkomCorpu.TourManager
                 targetFOV -= scroll * 20f;
                 targetFOV = Mathf.Clamp(targetFOV, minFOV, maxFOV);
 
-                //if (zoomSlider != null)
-                //    zoomSlider.SetValueWithoutNotify(Mathf.InverseLerp(minFOV, maxFOV, targetFOV));
+                if (zoomSlider != null)
+                    zoomSlider.SetValueWithoutNotify(Mathf.InverseLerp(minFOV, maxFOV, targetFOV));
             }
 
             cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * zoomSmooth);
@@ -188,15 +184,15 @@ namespace Tour360TelkomCorpu.TourManager
             autoRotate = !autoRotate;
         }
 
-        public void ZoomWithFallTransition(float cameraY, Action onStartTransition, Action onFinishTransition )
+        public void ZoomWithFallTransition(float cameraY, Action onStartTransition, Action onFinishTransition)
         {
             onStartTransition?.Invoke();
             // Convert 0–1 to 0–360
 
             float targetYaw = (cameraY >= 0f && cameraY <= 1f) //harus ada kondisi jika cameraY diluar 0-1 dan default nya dijadikan 0 
-                ? cameraY * 360f 
+                ? cameraY * 360f
                 : 0f;
-           
+
             float startAngle = 80f;
             float endAngle = 0f;
             float startHeight = 0.6f;
@@ -247,6 +243,5 @@ namespace Tour360TelkomCorpu.TourManager
                 Debug.Log("Drone animation finished");
             });
         }
-
     }
 }
